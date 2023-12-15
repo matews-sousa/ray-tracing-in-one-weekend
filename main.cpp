a0ca87fe5b8f0c155e5aeac6154f0d5ce8123474
Mateus Sousa <mateussdamiano@gmail.com>
2023-12-15 20:21:44 -0300

    Chapter 6 - Surfaces Normals and Multiple Objects

--- main.cpp
+++ main.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
 #include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "sphere.h"
+#include "rtweekend.h"
+#include "interval.h"
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
-  vec3 oc = r.origin() - center;
-  auto a = dot(r.direction(), r.direction());
-  auto b = 2.0 * dot(oc, r.direction());
-  auto c = dot(oc, oc) - radius*radius;
-  auto discriminant = b*b - 4*a*c;
-  return (discriminant >= 0);
-}
-
-color ray_color(const ray& r) {
-  if (hit_sphere(point3(0,0,-1), 0.5, r))
-    return color(1, 0, 0);
+color ray_color(const ray& r, const hittable& world) {
+  hit_record rec;
+  if (world.hit(r, interval(0, infinity), rec)) {
+    return 0.5 * (rec.normal + color(1, 1, 1));
+  }
 
   vec3 unit_direction = unit_vector(r.direction());
   auto a = 0.5 * (unit_direction.y() + 1.0);
@@ -29,6 +25,11 @@ int main() {
   // calculate the image height and ensure that it's at least 1
   int image_height = static_cast<int>(image_width / aspect_ratio);
   image_height = (image_height < 1) ? 1 : image_height;
+
+  // World
+  hittable_list world;
+  world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+  world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
   // Camera
   double focal_length = 1.0;
@@ -58,7 +59,7 @@ int main() {
       auto ray_direction =  pixel_center - camera_center;
       ray r(camera_center, ray_direction);
 
-      color pixel_color = ray_color(r);
+      color pixel_color = ray_color(r, world);
       write_color(std::cout, pixel_color);
     }
   }
