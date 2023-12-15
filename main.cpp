c9836842fe48bb73e195e1ded3fa57ce01c35a59
Mateus Sousa <mateussdamiano@gmail.com>
2023-12-15 17:08:12 -0300

    adding a simple red sphere

--- main.cpp
+++ main.cpp
@@ -3,10 +3,21 @@
 #include "ray.h"
 #include "vec3.h"
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+  vec3 oc = r.origin() - center;
+  auto a = dot(r.direction(), r.direction());
+  auto b = 2.0 * dot(oc, r.direction());
+  auto c = dot(oc, oc) - radius*radius;
+  auto discriminant = b*b - 4*a*c;
+  return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
+  if (hit_sphere(point3(0,0,-1), 0.5, r))
+    return color(1, 0, 0);
+
   vec3 unit_direction = unit_vector(r.direction());
   auto a = 0.5 * (unit_direction.y() + 1.0);
-
   return (1.0-a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
@@ -27,7 +38,7 @@ int main() {
 
   // calculate the vectors across the horizontal and down the vertical viewport edges.
   vec3 viewport_u = vec3(viewport_width, 0, 0);
-  vec3 viewport_v = vec3(-0, -viewport_height, 0);
+  vec3 viewport_v = vec3(0, -viewport_height, 0);
 
   // calculate the horizontal and vertical delta vectors from pixel to pixel
   vec3 pixel_delta_u = viewport_u / image_width;
